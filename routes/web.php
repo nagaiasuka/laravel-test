2183e0e7167b6d925a152e548142682014f47da7
nagaiasuka <asu.naga369@gmail.com>
2024-05-13 21:33:52 +0900

    登録・編集機能実装

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/list', [TodoListController::class, 'index']);
+Route::resource('tasks', TaskController::class);
